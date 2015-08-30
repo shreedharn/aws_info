a05c4971c36a52bee7dd9a3d237d5065835c71a1
Shreedhar Natarajan <shreedharn@gmail.com>
2015-08-30 16:58:57 -0400

    Volume query state modified

--- ec2_info.rb
+++ ec2_info.rb
@@ -1,6 +1,6 @@
 class Ec2Info
   INSTANCE_STATES =  ['pending','running', 'shutting-down', 'stopping', 'stopped']
-  VOLUME_STATES =   ['creating','available','in-use','deleting','error']
+  VOLUME_STATES =   ['creating','available','deleting','error']
 
   def initialize(regions, credentials)
     @regions = regions
@@ -20,7 +20,7 @@ class Ec2Info
   def print_instance_count(region, ec2_client)
     describe_instances_results = ec2_client.describe_instances({filters: [{name: 'instance-state-name',values: INSTANCE_STATES}]}).data
     instance_count = describe_instances_results.reservations.count
-    puts "\tRunning Ec2 Instance count: #{instance_count}"
+    puts "\tEc2 Instance count: #{instance_count}"
   end
   def print_volume_count(region,ec2_client)
     describe_volume_results = ec2_client.describe_volumes({filters: [{name: 'status',values: VOLUME_STATES}]}).data
